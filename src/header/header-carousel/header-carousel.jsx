0565a16e1efde8bb3fefa6a7fbedc868e982bd0d
Jamoladdin <jamoladdinisnatdinov2@gmail.com>
2023-11-09 23:20:15 +0500

    updateing with carousel

--- src/header/header-carousel/header-carousel.jsx
+++ src/header/header-carousel/header-carousel.jsx
@@ -1,32 +1,28 @@
-import "slick-carousel/slick/slick.css";
-import "slick-carousel/slick/slick-theme.css";
+import { Image } from '@chakra-ui/react';
+import React from 'react';
+import { Slide } from 'react-slideshow-image';
+import 'react-slideshow-image/dist/styles.css';
 
-import React, { Component } from "react";
-import Slider from "react-slick";
-import { Image } from "@chakra-ui/react";
-import Banner from './banner1.png'
+const Example = () => {
+    const images = [
+        "https://images.unsplash.com/photo-1509721434272-b79147e0e708?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1500&q=80",
+        "https://images.unsplash.com/photo-1506710507565-203b9f24669b?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1536&q=80",
+        "https://images.unsplash.com/photo-1536987333706-fc9adfb10d91?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1500&q=80",
+    ];
 
-export default class SimpleSlider extends Component {
-    render() {
-        const settings = {
-            arrow:false,
-            dots: true,
-            infinite: true,
-            speed: 550,
-            slidesToShow: 1,
-            slidesToScroll: 1
-        };
-        return (
-            <div>
-                <Slider marginTop={'30px'} {...settings}>
-                    <Image cursor={'pointer'} src={Banner}/>
-                    <Image cursor={'pointer'} src={Banner}/>
-                    <Image cursor={'pointer'} src={Banner}/>
-                    <Image cursor={'pointer'} src={Banner}/>
-                    <Image cursor={'pointer'} src={Banner}/>
-                    <Image cursor={'pointer'} src={Banner}/>
-                </Slider>
+    return (
+        <Slide>
+            <div className="each-slide-effect">
+                <Image w={'100%'} h={{base:'200px', sm:'240px', md:'270px', lg:'300px', xl:'340px', '2xl':'370px'}} objectFit={'cover'} src='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSTWYenGebfzJCuwiR4WdjzTzI7BdavwbbeHA&usqp=CAU' />
             </div>
-        );
-    }
-}
+            <div className="each-slide-effect">
+                <Image w={'100%'} h={{base:'200px', sm:'240px', md:'270px', lg:'300px', xl:'340px', '2xl':'370px'}} objectFit={'cover'} src='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR_hGXrY82OE77Fk4mYFcRabjSHexcJRt-p4w&usqp=CAU' />
+            </div>
+            <div className="each-slide-effect">
+                <Image w={'100%'} h={{base:'200px', sm:'240px', md:'270px', lg:'300px', xl:'340px', '2xl':'370px'}} objectFit={'cover'} src=' https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS95LYR1YwzavMkK8ZjwDwauhW7vUMU_FFsWg&usqp=CAU' />
+            </div>
+        </Slide>
+    );
+};
+
+export default Example;
--- src/pages/page1/App.jsx
+++ src/pages/page1/App.jsx
@@ -2,7 +2,7 @@ import {
   Box,
   Heading
 } from '@chakra-ui/react'
-import Header from '../../header/header'
+import Swiper from '../../header/header-carousel/header-carousel'
 import Katalog from './katalog'
 import WeWorkWith from './card2'
 import AboutUs from './aboutUs'
@@ -11,7 +11,8 @@ function App() {
 
   return (
     <>
-        <Box textAlign={'center'} padding={{ base: '80px', sm: '100px' }} w={'100%'} h={{ base: '200px', sm: '230px', md: '260px', lg: '280px', xl: '300px' }} bg={'violet'}><Heading> Banner </Heading></Box>
+        <Swiper />
+        {/* <Box textAlign={'center'} padding={{ base: '80px', sm: '100px' }} w={'100%'} h={{ base: '200px', sm: '230px', md: '260px', lg: '280px', xl: '300px' }} bg={'violet'}><Heading> Banner </Heading></Box> */}
         <Katalog />
         < WeWorkWith />
         <AboutUs />
