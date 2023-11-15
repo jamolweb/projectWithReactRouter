cdb71757a297aa8abc091284afba162b24023bc4
Jamoladdin <jamoladdinisnatdinov2@gmail.com>
2023-11-15 17:31:07 +0500

    upadating with plus minus prices

--- src/pages/cart.jsx
+++ src/pages/cart.jsx
@@ -12,39 +12,48 @@ import {
 } from "@chakra-ui/react";
 import { useState } from "react";
 const Cart = () => {
-
-    const { Product, setProduct,  } = useContext(Products);
-
+    const { Product, setProduct, } = useContext(Products);
     let remove = (id) => {
-        let newData = Product.filter(item => item.id !== id)
-        setProduct(newData)
+        let newData = count.filter(item => item.id !== id)
+        setCount(newData)
     }
 
-    let [state, setState] = useState('')
+    let arr =
+        Product.map(elem => {
+            return {
+                ...elem,
+                count: 1,
+            }
+        });
 
-    // countes
+    let [count, setCount] = useState(arr)
 
-    let arr = [];
-
-
-    let decCrease = (id) => {
-        // for (let i = 0; i < arr.length; i++) {
-        //     console.log(arr[i]);
-        // }
-
-        setState(arr)
-
-        setState((state)=> {
-        return state.map((product)=>{
-            if(product.id===id){
+    const decCrease = (id) => {
+        let ttt = count.map(elem => {
+            if (elem.id === id) {
                 return {
-                    ...product,
-                    count:++product.count
+                    ...elem,
+                    count: elem.count + 1,
                 }
             }
+            return elem;
         })
-        })
+        setCount(ttt);
     }
+    const inCrease = (id) => {
+        let ttt = count.map(elem => {
+            if (elem.id === id && elem.count > 1) {
+                return {
+                    ...elem,
+                    count: elem.count - 1,
+                }
+            }
+            return elem;
+        })
+        setCount(ttt);
+    }
+
+    let totalPrice = 0;
 
 
 
@@ -54,47 +63,44 @@ const Cart = () => {
             <Context>
                 <Grid>
                     {
-                        Product.map(item => {
-                            let obj = {
-                                id: item.id,
-                                price: item.price,
-                                count: 1,
-                                totalPrice: item.price,
-                            }
-                            arr.push(obj)
-
-                            let {id, price, count, totalPrice} = obj
+                        count.map((item) => {
+                            let localPrice = item.price * item.count
+                            totalPrice += localPrice;
                             return (
                                 <Box mt={'10px'} flexWrap={'wrap'} border={'1px solid grey !important'} key={item.id}>
                                     <GridItem gap={'40px'} padding={'20px'} display={{ base: "block", lg: 'flex' }} >
-                                        <Image w={{ base: '100px', lg: '150px' }} h={'220px'} objectFit={'cover'} src={item?.thumbnail} />
-                                        <Flex w={'100%'} alignItems={'center'} justifyContent={'space-between'}>
-                                            <Heading>
+                                        <Image w={{ base: '100%', lg: '150px' }} h={'220px'} objectFit={'cover'} src={item?.thumbnail} />
+                                        <Flex flexWrap={'wrap'} w={'100%'} alignItems={'center'} justifyContent={'space-between'}>
+                                            <Heading mt={'10px'} >
                                                 {item.title}
                                             </Heading>
+                                            <Flex justifyContent={'space-between'} w={{base:'100%', lg:'50%'}}>
                                             <Flex gap={'20px'} justifyContent={'center'}>
-                                                <Button>-</Button>
+                                                <Button onClick={() => inCrease(item.id)}>-</Button>
                                                 <Heading size={'lg'}>
                                                     {
-                                                        count
+                                                        item.count
                                                     }
                                                 </Heading>
                                                 <Button onClick={() => decCrease(item.id)}>+</Button>
                                             </Flex>
-                                            <Heading size={'lg'}>$ {item.price}</Heading>
+                                            <Heading size={'lg'}>$ {localPrice}</Heading>
                                             <Button
                                                 onClick={() => remove(item.id)}
                                                 w={'30'}
                                                 color={'grey'}
                                             >x</Button>
+                                            </Flex>
                                         </Flex>
                                     </GridItem>
-
                                 </Box>
                             )
                         })
                     }
                 </Grid>
+                <Flex justifyContent={'end'}>
+                    <Box w={{base:'80%',sm:'320px'}} padding={'20px'} borderRadius={'10px'} fontSize={'30px'} border={'1px solid black'} mt={'20px'} display={'flex'}>Total price : <Heading color={''} ml={'4px'}> $ {totalPrice}</Heading></Box>
+                </Flex>
             </Context>
         </>
     )
